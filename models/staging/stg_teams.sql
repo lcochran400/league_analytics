caf4863f084387ad8603d60ff26dd1037a551507
unknown <lcochran400@gmail.com>
2025-09-23 20:08:14 -0400

    change horde to grubs and reorder columns in stg_teams

--- models/staging/stg_teams.sql
+++ models/staging/stg_teams.sql
@@ -23,23 +23,25 @@ renamed as (
         json_extract(team_json, '$.feats.FIRST_BLOOD.featState') as warfare_feat_count,
         json_extract(team_json, '$.feats.FIRST_TURRET.featState') as first_turret_feat_count,
 
-        -- objectives
-        json_extract(objectives_json, '$.atakhan.first') as has_slain_first_atakhan,
-        json_extract(objectives_json, '$.atakhan.kills') as total_atakhan_kills,
-        json_extract(objectives_json, '$.baron.first') as has_slain_first_baron,
-        json_extract(objectives_json, '$.baron.kills') as total_baron_kills,
+        -- objective booleans
         json_extract(objectives_json, '$.champion.first') as has_slain_first_champion,
-        json_extract(objectives_json, '$.champion.kills') as total_champion_kills,
-        json_extract(objectives_json, '$.dragon.first') as has_slain_first_dragon,
-        json_extract(objectives_json, '$.dragon.kills') as total_dragon_kills,
-        json_extract(objectives_json, '$.horde.first') as has_slain_first_grub,
-        json_extract(objectives_json, '$.horde.kills') as total_horde_kills,
-        json_extract(objectives_json, '$.inhibitor.first') as has_slain_first_inhibitor,
-        json_extract(objectives_json, '$.inhibitor.kills') as total_inhibitor_kills,
-        json_extract(objectives_json, '$.riftHerald.first') as has_slain_first_rift_herald,
-        json_extract(objectives_json, '$.riftHerald.kills') as total_rift_herald_kills,
         json_extract(objectives_json, '$.tower.first') as has_slain_first_tower,
-        json_extract(objectives_json, '$.tower.kills') as total_tower_kills
+        json_extract(objectives_json, '$.inhibitor.first') as has_slain_first_inhibitor,
+        json_extract(objectives_json, '$.dragon.first') as has_slain_first_dragon,
+        json_extract(objectives_json, '$.horde.first') as has_slain_first_grub,
+        json_extract(objectives_json, '$.riftHerald.first') as has_slain_first_rift_herald,
+        json_extract(objectives_json, '$.atakhan.first') as has_slain_first_atakhan,
+        json_extract(objectives_json, '$.baron.first') as has_slain_first_baron,
+
+        -- objective totals
+        json_extract(objectives_json, '$.champion.kills') as total_champion_kills,
+        json_extract(objectives_json, '$.tower.kills') as total_tower_kills,
+        json_extract(objectives_json, '$.inhibitor.kills') as total_inhibitor_kills,
+        json_extract(objectives_json, '$.dragon.kills') as total_dragon_kills,
+        json_extract(objectives_json, '$.horde.kills') as total_grub_kills,
+        json_extract(objectives_json, '$.riftHerald.kills') as total_rift_herald_kills,
+        json_extract(objectives_json, '$.atakhan.kills') as total_atakhan_kills,
+        json_extract(objectives_json, '$.baron.kills') as total_baron_kills
 
         from source
 
